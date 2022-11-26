f016fa61a918a9931e9b605a3eeb7a48bb332b4e
Okan <okanacioglu@gmail.com>
2022-11-26 12:34:34 +0300

    input-search-box-component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends Component {
     this.state = {
       monsters: [],
     }
-    console.log('1')
+    console.log('constructor')
   }
   componentDidMount() {
-    console.log('3')
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
@@ -24,9 +24,17 @@ class App extends Component {
   }
 
   render() {
-    console.log('2')
+    console.log('render')
     return (
       <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            console.log(event)
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
